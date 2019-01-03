9da74435d23ff8407c38e903887d2cb949aa69fc
Joe Yang <joe.yang.design@gmail.com>
2019-01-03 08:20:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 function dwarfRollCall(dwarves) {
+  var dwarfNames = [];
+  
+  for (let i = 0; i < dwarves.length; i++) {
+    dwarfNames.push(`${i + 1}. ${dwarves[i]} `)
+  }
+  
+  return `${dwarfNames.join('')}`
+  
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var calls = [];
+  
+  for (let i = 0; i < planeteerCalls.length; i++) {
+    calls.push(`${planeteerCalls[i].toUpperCase()}!`)
+  }
+   return calls;
+  
 }
 
 function longPlaneteerCalls(words) {
+  
+  var n = words.length;
+  if (n > 4) {
+    return true
+  }
+  
+  else {
+    return false
+  }
 }
 
 function findTheCheese (foods) {
+  let cheese = ["cheddar", "gouda","camembert"]
+  for (let i = 0; i < foods.length; i++) {
+    if (foods[i] === cheese[0] || cheese[1] || cheese[2]) {
+     return cheese[i];
+    }
+ 
+  }
+    
+  
 }
+
